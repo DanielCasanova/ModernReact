8c28e83cd9ccf3e4a4ef36b5fed71cbd4c212905
Danny <nanaeacp@gmail.com>
2019-12-19 12:27:55 +0000

    Finished components exercise.

--- Section 03/components/src/index.js
+++ Section 03/components/src/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () =>
 {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar"/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadta">
-                        <span className="date">Today at 18h00</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ApprovalCard>
+                Are you sure you want to do this?
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 16h45" comment="Nice blog post!" avatar={Faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Joe" timeAgo="Today at 12h30" comment="Awesome!" avatar={Faker.image.avatar()}/>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Ann" timeAgo="Today at 10h20" comment="Really cool!" avatar={Faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 };
