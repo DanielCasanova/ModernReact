cf110ba7d26757770050d34a2fb6f66906a28050
Danny <nanaeacp@gmail.com>
2020-01-24 13:10:54 +0000

    Base react app for blog project and initial setup.

---
+++ Section 14/blog/src/components/PostList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostList extends React.component
+{
+    render()
+    {
+        return (
+            <div>
+                Post List
+            </div>
+        );
+    }
+}
+
+export default PostList;
