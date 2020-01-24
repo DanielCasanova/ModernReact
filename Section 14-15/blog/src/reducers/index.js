7cfa8643f49e3c96a7e53f6efe2a759f6965df96
Danny <nanaeacp@gmail.com>
2020-01-24 17:58:47 +0000

    Added post reducer

--- Section 14-15/blog/src/reducers/index.js
+++ Section 14-15/blog/src/reducers/index.js
@@ -1,6 +1,7 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
 
 export default combineReducers(
 {
-    dummy: () => 0
+    posts: postsReducer
 });
